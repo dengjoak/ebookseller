226ed1588515ec25256c46db2cb9d0bb5dff4fbf
Deng Wuor Joak <dengjoak@gmail.com>
2019-06-03 21:17:54 +0200

    yes

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const stripe = require("stripe")("sk_test_NcDUYTv1mtabszdU0OLHDu3L00JlaV5oV2");
+
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+// Handelbars Middleware
+app.engine(`handlebars`, exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Static Folder
+app.use(
+  express.static(`
+${__dirname}/public`)
+);
+
+// Index Route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
